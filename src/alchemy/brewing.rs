2a0c1e51ab1051ebad0e21b39d1df6483dea818b
TLovell <trevorlovelldesign@gmail.com>
2021-08-28 19:47:45 -0600

    Added Cauldron flag component and get_reactive_compounds

--- src/alchemy/brewing.rs
+++ src/alchemy/brewing.rs
@@ -1,4 +1,5 @@
 use crate::alchemy::compounds::Compound;
+use bevy::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Hash, Serialize, Deserialize)]
@@ -23,4 +24,28 @@ pub struct ReactionRule {
     pub heat: Option<Heat>,
     /// Setting to None means this compound reacts under any stir method
     pub stir_method: Option<StirMethod>,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash, Serialize, Deserialize)]
+struct Cauldron;
+
+fn get_reactive_compounds(
+    reaction_rules: Vec<ReactionRule>,
+    stir_method: StirMethod,
+    heat: Heat,
+) -> Vec<Compound> {
+    reaction_rules
+        .clone()
+        .into_iter()
+        .filter(|rule| {
+            (match rule.heat {
+                Some(h) => h == heat,
+                None => true,
+            }) && (match rule.stir_method {
+                Some(sm) => sm == stir_method,
+                None => true,
+            })
+        })
+        .map(|rule| rule.compound)
+        .collect::<Vec<Compound>>()
 }
