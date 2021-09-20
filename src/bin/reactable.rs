45b7119d43d6dd7ef9cfe3bcb3146202fcd85adb
TLovell <trevorlovelldesign@gmail.com>
2021-09-19 19:09:30 -0600

    Finished writing rows for reactable

--- src/bin/reactable.rs
+++ src/bin/reactable.rs
@@ -14,6 +14,25 @@ fn main() -> io::Result<()> {
             alchemy::systems::get_reactive_compounds(&reaction_rules, *stir_method, *heat);
 
         let mut row = vec![row_compound.to_string()];
+
+        for alchemy::resources::ReactionRule {
+            compound: col_compound,
+            ..
+        } in &reaction_rules
+        {
+            if reactive_compounds.contains(col_compound) {
+                row.push(
+                    row_compound
+                        .list_possible_reactions(col_compound)
+                        .into_iter()
+                        .map(|(left, right)| format!("{}+{}", left, right))
+                        .collect::<Vec<String>>()
+                        .join(", "),
+                )
+            } else {
+                row.push("".to_string())
+            }
+        }
     }
     Ok(())
 }
